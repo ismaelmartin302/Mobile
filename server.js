675d0b8fe4c75c8abbcfdb56f8c56607fb065f9a
isma <ismaelmartin302@gmail.com>
2024-09-29 00:53:27 +0200

    "Deleted files classes.css, classes.scss, and index.html; modified server.js to handle SCSS files and updated file paths."

--- server.js
+++ server.js
@@ -5,9 +5,25 @@ const path = require('node:path');
 const desiredPort = process.env.PORT ?? 1234;
 
 const server = http.createServer((req, res) => {
-    console.log('Petición recibida', req.url);
-    if (req.url === '/') {
-        const filePath = path.join(__dirname, 'index.html');
+    console.log('Petición recibida:', req.url);
+
+    if (req.url.endsWith('.scss')) {
+        const cssPath = path.join(__dirname, `app/${req.url}`);
+        fs.readFile(cssPath, (err, data) => {
+            if (err) {
+                console.error('Error al leer el archivo CSS:', err);
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Error interno del servidor');
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/css');
+                res.end(data);
+            }
+        });
+    }
+    else if (req.url === '/') {
+        const filePath = path.join(__dirname, 'app/index.html');
         fs.readFile(filePath, (err, data) => {
             if (err) {
                 console.error('Error al leer el archivo:', err);
@@ -17,15 +33,16 @@ const server = http.createServer((req, res) => {
             } else {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'text/html');
-                res.end(data)
+                res.end(data);
             }
         });
-    } else {
+    }
+    else {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/plain');
         res.end('Página no encontrada');
     }
-})
+});
 
 server.listen(desiredPort, () => {
     console.log(`Servidor escuchando en el puerto http://localhost:${desiredPort}`);
