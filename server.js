c75a07a015595fe0ed76ebd0cc31fef54b938445
isma <ismaelmartin302@gmail.com>
2024-09-28 23:49:15 +0200

    Creating server

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('node:http');
+
+const desiredPort = process.env.PORT ?? 1234;
+
+const server = http.createServer((req, res) => {
+    res.end('Hola');
+    console.log('Petición recibida');
+})
+
+server.listen(desiredPort, () => {
+    console.log(`Servidor escuchando en el puerto http://localhost:${desiredPort}`);
+})
