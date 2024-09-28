e44a4d098a50241acf2d23c0730bbcad811241c2
isma <ismaelmartin302@gmail.com>
2024-09-29 00:13:32 +0200

    "Added file system module and modified server to serve index.html file"

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const http = require('node:http');
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 1234;
 
 const server = http.createServer((req, res) => {
-    res.end('Hola');
-    console.log('Petición recibida');
+    console.log('Petición recibida', req.url);
+    if (req.url === '/') {
+        fs.readFile('./index.html', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                res.end(data)
+            }
+        })
+    } else {
+        res.end('Error')
+    }
 })
 
 server.listen(desiredPort, () => {
